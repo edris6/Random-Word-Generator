96cb5ac07f46813372ac376a81dfc28f50e7b718
badonyt <soccerskin87@gmail.com>
2022-08-29 23:08:58 +0100

    Impossible to get two times the same name

--- readme.md
+++ readme.md
@@ -2,6 +2,15 @@
 
 This is a small project, about generating random names in this case swords, because of the url that i set in netlify
 
+# Contribute
+
+If you would like to contribute just add random sword names into the **array / list**(whatever you call it).
+
+# Question
+
+**Why Swords**
+Look at the url
+
 # Hosting
 
 We hosted this website with netlify, thanks!
--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,18 @@ let main = [
 	"Harpe",
 	"Ame-no-Habakiri",
 	"Kusanagi-no-Tsurugi",
+	"Xiphos",
+	"Kopis",
 ];
 let randomnumberr = 0;
+let previousnumber = 100;
 function getrandomword() {
 	randomnumberr = Math.floor(Math.random() * main.length);
-	console.log(main[randomnumberr]);
+	if(previousnumber == randomnumberr){
+		getrandomword();
+		return;
+	}
+	console.log(main[randomnumberr] + ", The random number is "+randomnumberr);
 	document.getElementById("display-text").innerHTML = main[randomnumberr];
+	previousnumber = randomnumberr;
 }
